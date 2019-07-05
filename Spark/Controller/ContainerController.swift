c02abffc79e017e99445dddd1455f8fb4e3ce933
Dmitry Chuveev <dchuveev@chegg.com>
2019-07-04 18:55:10 -0700

    created folders and 3 controllers classes

---
+++ Spark/Controller/ContainerController.swift
@@ -0,0 +1,23 @@
+//
+//  ContainerController.swift
+//  Spark
+//
+//  Created by Dmitry Chuveev on 7/4/19.
+//  Copyright © 2019 Dmitry Chuveev. All rights reserved.
+//
+
+import UIKit
+
+class ContainerController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Handlers
+    
+}
---
+++ Spark/Controller/MenuController.swift
@@ -0,0 +1,23 @@
+//
+//  MenuController.swift
+//  Spark
+//
+//  Created by Dmitry Chuveev on 7/4/19.
+//  Copyright © 2019 Dmitry Chuveev. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Handlers
+    
+}
