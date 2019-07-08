643c4fbc64505ff55319e369c7869665d2da99cc
Dmitry Chuveev <dmitry_chuveev@Dmitrys-Mac-mini.local>
2019-07-07 20:30:11 -0700

    Created login veiw with elements

---
+++ Spark/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Spark
+//
+//  Created by Dmitry Chuveev on 7/7/19.
+//  Copyright © 2019 Dmitry Chuveev. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Spark/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Spark
+//
+//  Created by Dmitry Chuveev on 7/7/19.
+//  Copyright © 2019 Dmitry Chuveev. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
