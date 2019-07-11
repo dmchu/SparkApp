49de7222a432c6a7e8a72f8ab2c5f4bf848747a6
Dmitry Chuveev <dmitry_chuveev@Dmitrys-Mac-mini.local>
2019-07-11 05:57:58 -0700

    created profile vc with xib file

---
+++ Spark/Controller/ProfileVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileVC.swift
+//  Spark
+//
+//  Created by Dmitry Chuveev on 7/11/19.
+//  Copyright © 2019 Dmitry Chuveev. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
