885b187b493f15fd630bc3181865530015cbc8eb
Dmitry Chuveev <dmitry_chuveev@Dmitrys-Mac-mini.local>
2019-07-11 06:04:24 -0700

    created outlets and actions for ui elements in profile view

--- Spark/Controller/ProfileVC.swift
+++ Spark/Controller/ProfileVC.swift
@@ -10,21 +10,29 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
+    // Outlets
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var userEmail: UILabel!
+    
+    @IBOutlet weak var bgView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+    }
+    
+    
+    
 }
