501b0288193d0c92f3f054fba17d9784daa3333c
Dmitry Chuveev <dmitry_chuveev@Dmitrys-Mac-mini.local>
2019-07-11 09:32:13 -0700

    completed ChannelVC

--- Spark/Model/Channel.swift
+++ Spark/Model/Channel.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Channel : Decodable {
+    public private(set) var _id: String!
+    public private(set) var name: String!
+    public private(set) var description: String!
+    public private(set) var __v: Int?
+}
+
+
+// If working with SwiftyJSON
+//struct Channel {
+//    public private(set) var channelTitle: String!
+//    public private(set) var channelDescription: String!
+//    public private(set) var id: String!
+//}
