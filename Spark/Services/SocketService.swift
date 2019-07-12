b47bfe99964d1f40ab18b193f69f0d323b864c78
Dmitry Chuveev <dmitry_chuveev@Dmitrys-Mac-mini.local>
2019-07-12 05:59:01 -0700

    create channel

---
+++ Spark/Services/SocketService.swift
@@ -0,0 +1,39 @@
+//
+//  SocketService.swift
+//  Spark
+//
+//  Created by Dmitry Chuveev on 7/12/19.
+//  Copyright © 2019 Dmitry Chuveev. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+    
+    static let instance = SocketService()
+    
+    let manager: SocketManager
+    let socket: SocketIOClient
+    
+    
+    override init() {
+        self.manager = SocketManager(socketURL: URL(string: BASE_URL)!)
+        self.socket = manager.defaultSocket
+        super.init()
+    }
+
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+}
