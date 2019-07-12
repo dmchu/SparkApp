76853157b2ef0854bc3d9dfbd77c563c18cf1b09
Dmitry Chuveev <dmitry_chuveev@Dmitrys-Mac-mini.local>
2019-07-12 09:15:33 -0700

    created messages functionality

--- Spark/Utilities/Constants.swift
+++ Spark/Utilities/Constants.swift
@@ -17,6 +17,7 @@ let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
 let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 let URL_GET_CHANNELS = "\(BASE_URL)channel/"
+let URL_GET_MESSAGES = "\(BASE_URL)message/byChannel/"
 
 // Colors
 let sparkPurplePlaceholder = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 0.5035049229)
